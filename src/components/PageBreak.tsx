5bd1ce6e9db1c8acf26157f7aa75f71c4440a516
Nishant Gaharwar <iamnishantgaharwar@gmail.com>
2024-09-11 23:58:52 +0530

    Add PageBreak feature

--- src/components/PageBreak.tsx
+++ src/components/PageBreak.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const PageBreak: React.FC = () => {
-  return (
-    <div className="page-break" style={{ display: 'none' }}>
-      ---PAGEBREAK---
-    </div>
-  );
+  return <div>PAGEBREAK</div>;
 };
 
 export default PageBreak;
